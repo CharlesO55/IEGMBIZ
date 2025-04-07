9b30875e23a44b0956b8bec19798bde6e782faba
CharlesO55 <76197127+CharlesO55@users.noreply.github.com>
2025-04-07 20:10:41 +0800

    assets

--- Assets/_Scripts/GameObjects/Fixtures/FixtureLot.cs
+++ Assets/_Scripts/GameObjects/Fixtures/FixtureLot.cs
@@ -3,24 +3,23 @@ using System.Collections.Generic;
 using components.controllables;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider2D), typeof(SpriteRenderer))]
+[RequireComponent(typeof(Collider2D))]
 public class FixtureLot : MonoBehaviour, ITappable
 {
     [SerializeField] GameObject prefab;
     [SerializeField] int leafCost;
 
     
-    SpriteRenderer sRenderer;
+    [SerializeField] SpriteRenderer sRenderer;
     void Awake()
     {
-        sRenderer = GetComponent<SpriteRenderer>();
         sRenderer.color = Color.gray;
 
-        if(prefab.TryGetComponent<SpriteRenderer>(out SpriteRenderer c))
-        {
-            this.transform.localScale = c.transform.localScale;
-            this.sRenderer.sprite = c.sprite;
-        }
+        //if(prefab.TryGetComponent<SpriteRenderer>(out SpriteRenderer c))
+        //{
+        //    this.transform.localScale = c.transform.localScale;
+        //    this.sRenderer.sprite = c.sprite;
+        //}
 
         RegisterToManager();
     }
@@ -41,7 +40,7 @@ public class FixtureLot : MonoBehaviour, ITappable
     public void UpdatePurchasability()
     {
         bool affordable = PlayerProgress.I.CanBuy(leafCost);
-        sRenderer.color = affordable ? Color.grey : Color.black;
+        sRenderer.color = affordable ? Color.white : Color.grey;
     }
 
     public void onUserInput(TouchArgs e)
